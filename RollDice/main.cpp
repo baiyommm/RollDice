1a81da27370e1ccf6a99d8a903f9f926cd5d5a78
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-09-15 03:07:35 -0700

    Input the libraries, and the code for random seed/number (srand) and the initialized values min/max = 1-6,

--- RollDice/main.cpp
+++ RollDice/main.cpp
@@ -1,14 +1,19 @@
-//
-//  main.cpp
-//  RollDice
-//
-//  Created by Chatchapong Thuaypha on 9/15/24.
-//
-
 #include <iostream>
+#include <cstdlib> // for the "random" function
+#include <ctime> // for the time function
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main () {
+    
+    // Code needed to initialize a random number
+    srand(static_cast<unsigned>(time(0)));
+    
+    // Initializing the min/max values of the Dice
+    int minValue = 1;
+    int maxValue = 6;
+    
+    
+    
     return 0;
 }
